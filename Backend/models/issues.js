27558d8fc54a50fae14834321b669a24ca723eed
bsaikiran618 <bsaikiran618@gmail.com>
2021-03-27 18:36:52 +0530

    Added models of Issues, Volunteers, Patrons

---
+++ Backend/models/issues.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const issuesSchema = new mongoose.Schema({
+	title:		{type:String, required:true},
+	description:{type:String, required:true},
+	location:{
+		type:{
+			type: String,
+			enum: ['Point'];
+		},
+		coordinates:{
+			type:[Number],
+			required:true
+		}
+	},
+	backers:{type:Number, required:true},
+	targetFund:{type: Number, required: true}
+});
+
+
+const issues = mongoose.model('issues', issuesSchema);
+
+module.exports = issues;
