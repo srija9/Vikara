de452a92c7d50ee9bdabf3afae3af354fa9b8bd1
bsaikiran618 <bsaikiran618@gmail.com>
2021-03-28 14:09:24 +0530

    .

--- Backend/issues.js
+++ Backend/issues.js
@@ -6,4 +6,10 @@ router.use(tokenAuth); // All issues endpoints must be authenticated
 
 //issues endpoints;
 
+	//	creating a new issue
+	//	back an issue
+	//	fund an issue
+	//	delete an issue
+	//	add media
+
 module.exports = router;
--- Backend/users.js
+++ Backend/users.js
@@ -4,6 +4,7 @@ const tokenAuth = require("./tokenAuth");
 
 router.use(tokenAuth); //All user management endpoints are token authenticated
 //User endpoints
+
 	//Change Password
 	//Change email
 	//Change phone
